7e76554ec298a46c6b108134a1641a330af6c6c6
Soyuj Jung Basnet <bsoyuj@gmail.com>
2023-10-05 02:10:58 -0400

    4 bit quantization code

---
+++ src/llama2/finetune/finetune_4bit.py
@@ -0,0 +1,113 @@
+import torch
+from datasets import load_dataset, Features, Value
+from peft import LoraConfig
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    TrainingArguments,
+)
+from trl import SFTTrainer
+
+from src.utils.defaults import DATA_DIR, LLAMA_HUGGINGFACE_CHECKPOINT, DEEP_IMPACT_DIR, DEVICE
+
+context_feat = Features({'text': Value(dtype='string', id=None)})
+train_data = load_dataset("json", data_files=str(DEEP_IMPACT_DIR / "train.jsonl"), split="train")
+eval_data = load_dataset("json", data_files=str(DEEP_IMPACT_DIR / "test.jsonl"), split="train")
+
+model_name = str(LLAMA_HUGGINGFACE_CHECKPOINT)
+
+new_model = "doc2query-llama-2-7b"
+lora_r = 64
+lora_alpha = 16
+lora_dropout = 0.1
+
+use_4bit = True
+bnb_4bit_compute_dtype = "float16"
+bnb_4bit_quant_type = "nf4"
+use_nested_quant = False
+
+output_dir = str(DATA_DIR / "finetuned_4bit")
+num_train_epochs = 1
+fp16 = False
+bf16 = False
+per_device_train_batch_size = 4
+per_device_eval_batch_size = 4
+gradient_accumulation_steps = 1
+gradient_checkpointing = True
+max_grad_norm = 0.3
+learning_rate = 2e-4
+weight_decay = 0.001
+optim = "paged_adamw_32bit"
+lr_scheduler_type = "constant"
+max_steps = -1
+warmup_ratio = 0.03
+group_by_length = True
+save_steps = 25
+logging_steps = 5
+max_seq_length = None
+packing = False
+device_map = DEVICE
+
+compute_dtype = getattr(torch, bnb_4bit_compute_dtype)
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=use_4bit,
+    bnb_4bit_quant_type=bnb_4bit_quant_type,
+    bnb_4bit_compute_dtype=compute_dtype,
+    bnb_4bit_use_double_quant=use_nested_quant,
+)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    quantization_config=bnb_config,
+    device_map=device_map
+)
+model.config.use_cache = False
+model.config.pretraining_tp = 1
+tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+tokenizer.pad_token = tokenizer.eos_token
+tokenizer.padding_side = "right"
+peft_config = LoraConfig(
+    lora_alpha=lora_alpha,
+    lora_dropout=lora_dropout,
+    r=lora_r,
+    bias="none",
+    task_type="CAUSAL_LM",
+)
+
+# Set training parameters
+training_arguments = TrainingArguments(
+    output_dir=output_dir,
+    num_train_epochs=num_train_epochs,
+    per_device_train_batch_size=per_device_train_batch_size,
+    gradient_accumulation_steps=gradient_accumulation_steps,
+    optim=optim,
+    save_steps=save_steps,
+    logging_steps=logging_steps,
+    learning_rate=learning_rate,
+    weight_decay=weight_decay,
+    fp16=fp16,
+    bf16=bf16,
+    max_grad_norm=max_grad_norm,
+    max_steps=max_steps,
+    warmup_ratio=warmup_ratio,
+    group_by_length=group_by_length,
+    lr_scheduler_type=lr_scheduler_type,
+    report_to="all",
+    evaluation_strategy="steps",
+    eval_steps=5
+)
+# Set supervised fine-tuning parameters
+trainer = SFTTrainer(
+    model=model,
+    train_dataset=train_data,
+    eval_dataset=eval_data,
+    peft_config=peft_config,
+    dataset_text_field="text",
+    max_seq_length=max_seq_length,
+    tokenizer=tokenizer,
+    args=training_arguments,
+    packing=packing,
+)
+
+trainer.train()
+trainer.model.save_pretrained(new_model)
